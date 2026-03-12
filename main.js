0e5aa5d30767d8609f73d952ad15b882190713ce
Luckysunday-git <luckysunday001@gmail.com>
2026-03-12 08:33:13 +0100

    Adding new links

--- main.js
+++ main.js
@@ -1,156 +1,55 @@
 const SUBJECT_PAGES = {
-  basic_section: {
+  "basic section": {
     Basic1: {
-      "PHYSICAL & HEALTH EDUCATION":
-        "https://eaglecrestwood.modebills.com/physical-and-health-education-basic1/",
-      "PREVOCATIONAL STUDIES":
-        "https://eaglecrestwood.modebills.com/prevocational-studies-basic/",
-      "ENGLISH GRAMMAR":
-        "https://eaglecrestwood.modebills.com/english-grammar-basc-1/",
-      "CHRISTIAN RELIGIOUS STUDIES":
-        "https://eaglecrestwood.modebills.com/christian-religious-studies-basic-1/",
-
-      "VERBAL REASONING":
-        "https://eaglecrestwood.modebills.com/verbal-reasoning-basic1/",
-
-      "CODING & ROBOTICS":
-        "https://eaglecrestwood.modebills.com/coding-robotics-basic-1-2/",
+      "VOCATIONAL APTITUDE":
+        "https://eaglecrestwood.modebills.com/vocational-aptitude-basic-1/",
+      ICT: "https://eaglecrestwood.modebills.com/ict-basic1/",
     },
     Basic2: {
-      "PHYSICAL & HEALTH EDUCATION":
-        "https://eaglecrestwood.modebills.com/physical-and-health-education-basic2/",
-      "PREVOCATIONAL STUDIES":
-        "https://eaglecrestwood.modebills.com/prevocational-studies-basic2/",
-      "ENGLISH GRAMMAR":
-        "https://eaglecrestwood.modebills.com/english-grammar-basic-2/",
-      "CHRISTIAN RELIGIOUS STUDIES":
-        "https://eaglecrestwood.modebills.com/christian-religious-studies-basic-2/",
-      HISTORY: "https://eaglecrestwood.modebills.com/history-basic-2/",
-      "VERBAL REASONING":
-        "https://eaglecrestwood.modebills.com/verbal-reasoning-basic2/",
-      "CODING & ROBOTICS":
-        "https://eaglecrestwood.modebills.com/coding-robotics-basic-2/",
+      "VOCATIONAL APTITUDE":
+        "https://eaglecrestwood.modebills.com/vocational-aptitude-basic1-2/",
+      ICT: "https://eaglecrestwood.modebills.com/ict-basic2/",
     },
     Basic3: {
-      "PHYSICAL & HEALTH EDUCATION":
-        "https://eaglecrestwood.modebills.com/physical-and-health-education-basic3/",
-      "PREVOCATIONAL STUDIES":
-        "https://eaglecrestwood.modebills.com/prevocational-studies-basic3/",
-      "ENGLISH GRAMMAR":
-        "https://eaglecrestwood.modebills.com/english-grammar-basic-3/",
-      "CHRISTIAN RELIGIOUS STUDIES":
-        "https://eaglecrestwood.modebills.com/christian-religious-studies-basic-3/",
-      HISTORY: "https://eaglecrestwood.modebills.com/history-basic-3/",
-      "VERBAL REASONING":
-        "https://eaglecrestwood.modebills.com/verbal-reaoning-basic3/",
-      "CODING & ROBOTICS":
-        "https://eaglecrestwood.modebills.com/coding-robotics-basic-3/",
+      "VOCATIONAL APTITUDE":
+        "https://eaglecrestwood.modebills.com/vocational-apt-basic3/",
+      ICT: "https://eaglecrestwood.modebills.com/ict-basic3/",
     },
     Basic4: {
-      "PHYSICAL & HEALTH EDUCATION":
-        "https://eaglecrestwood.modebills.com/physical-and-health-education-basic4/",
-      "PREVOCATIONAL STUDIES":
-        "https://eaglecrestwood.modebills.com/prevocational-studies-basic4/",
-      "ENGLISH GRAMMAR":
-        "https://eaglecrestwood.modebills.com/english-grammar-basic-4/",
-      "CHRISTIAN RELIGIOUS STUDIES":
-        "https://eaglecrestwood.modebills.com/christian-religious-studies-basic-4/",
-      HISTORY: "https://eaglecrestwood.modebills.com/history-basic-4/",
-      "VERBAL REASONING":
-        "https://eaglecrestwood.modebills.com/verbal-reasoning-basic4/",
-      "CODING & ROBOTICS":
-        "https://eaglecrestwood.modebills.com/coding-robotics-basic-4/",
+      "VOCATIONAL APTITUDE":
+        "https://eaglecrestwood.modebills.com/vocational-apt-basic4/",
+      ICT: "https://eaglecrestwood.modebills.com/ict-basic4/",
     },
     Basic5: {
-      "PHYSICAL & HEALTH EDUCATION":
-        "https://eaglecrestwood.modebills.com/physical-and-health-education-basic5/",
-      "PREVOCATIONAL STUDIES":
-        "https://eaglecrestwood.modebills.com/prevocational-aptitude-basic5/",
-      "ENGLISH GRAMMAR":
-        "https://eaglecrestwood.modebills.com/english-grammar-basic-5-2/",
-      "CHRISTIAN RELIGIOUS STUDIES":
-        "https://eaglecrestwood.modebills.com/christian-religious-studies-basic-5/",
-      HISTORY: "https://eaglecrestwood.modebills.com/history-basic-5/",
-      "VERBAL REASONING":
-        "https://eaglecrestwood.modebills.com/verbal-reasoning-basic5/",
-      "CODING & ROBOTICS":
-        "https://eaglecrestwood.modebills.com/coding-robotics-basic-5/",
+      "VOCATIONAL APTITUDE":
+        "https://eaglecrestwood.modebills.com/vocational-apt-basic5/",
+      ICT: "https://eaglecrestwood.modebills.com/ict-basic5/",
     },
   },
-  jss_section: {
+  "jss section": {
     JSS1: {
-      HISTORY: "https://eaglecrestwood.modebills.com/history-jss1/",
-      "ENGLISH LANGUAGE":
-        "https://eaglecrestwood.modebills.com/english-language-jss1/",
-      "BASIC SCIENCE":
-        "https://eaglecrestwood.modebills.com/basic-science-jss1/",
+      "CULTURAL AND CREATIVE ARTS":
+        "https://eaglecrestwood.modebills.com/cca-jss1/",
+      "HOME ECONOMICS": "https://eaglecrestwood.modebills.com/econs-jss1/",
       "SOCIAL STUDIES":
-        "https://eaglecrestwood.modebills.com/social-studies-jss1/",
-      "CIVIC EDUCATION":
-        "https://eaglecrestwood.modebills.com/civic-education-jss1/",
-      "PHYSICAL & HEALTH EDUCATION":
-        "https://eaglecrestwood.modebills.com/physical-health-education-jss1/",
-      "AGRICULTURAL SCIENCE":
-        "https://eaglecrestwood.modebills.com/agricultural-science-jss1/",
-      "CODING & ROBOTICS":
-        "https://eaglecrestwood.modebills.com/coding-robotics-jss1/",
-      "LITERATURE-IN-ENGLISH":
-        "https://eaglecrestwood.modebills.com/literature-in-english-jss1/",
-      "COMPUTER STUDIES":
-        "https://eaglecrestwood.modebills.com/computer-science-jss1/",
-      "BASIC TECHNOLOGY":
-        "https://eaglecrestwood.modebills.com/basic-technology-jss1/",
+        "https://eaglecrestwood.modebills.com/socialstudies-jss1/",
     },
     JSS2: {
-      HISTORY: "https://eaglecrestwood.modebills.com/history-jss2/",
-      "ENGLISH LANGUAGE":
-        "https://eaglecrestwood.modebills.com/english-language-jss2-2/",
-      "BASIC SCIENCE":
-        "https://eaglecrestwood.modebills.com/basic-science-jss2/",
+      "CULTURAL AND CREATIVE ARTS":
+        "https://eaglecrestwood.modebills.com/cca-jss2/",
+      "HOME ECONOMICS": "https://eaglecrestwood.modebills.com/homecons-jss3/",
       "SOCIAL STUDIES":
-        "https://eaglecrestwood.modebills.com/social-studies-jss2/",
-
-      "CIVIC EDUCATION":
-        "https://eaglecrestwood.modebills.com/civic-education-jss2/",
-      "PHYSICAL & HEALTH EDUCATION":
-        "https://eaglecrestwood.modebills.com/physical-and-health-education-jss2/",
-      "AGRICULTURAL SCIENCE":
-        "https://eaglecrestwood.modebills.com/agricultural-science-jss2/",
-      "CODING & ROBOTICS":
-        "https://eaglecrestwood.modebills.com/coding-robotics-jss2/",
-      "LITERATURE-IN-ENGLISH":
-        "https://eaglecrestwood.modebills.com/literature-in-english-jss2/",
-      "COMPUTER SCIENCE":
-        "https://eaglecrestwood.modebills.com/computer-science-jss2/",
-      "BASIC TECHNOLOGY":
-        "https://eaglecrestwood.modebills.com/basic-technology-jss2/",
+        "https://eaglecrestwood.modebills.com/socialstudies-jss2/",
     },
     JSS3: {
-      HISTORY: "https://eaglecrestwood.modebills.com/history-jss3/",
-      "ENGLISH LANGUAGE":
-        "https://eaglecrestwood.modebills.com/english-language-jss3/",
-      "BASIC SCIENCE":
-        "https://eaglecrestwood.modebills.com/basic-science-jss3/",
+      "CULTURAL AND CREATIVE ARTS":
+        "https://eaglecrestwood.modebills.com/cca-jss3/",
+      "HOME ECONOMICS": "https://eaglecrestwood.modebills.com/honecons-jss3/",
       "SOCIAL STUDIES":
-        "https://eaglecrestwood.modebills.com/social-studies-jss3/",
-      "CIVIC EDUCATION":
-        "https://eaglecrestwood.modebills.com/civic-education-jss3/",
-
-      "PHYSICAL & HEALTH EDUCATION":
-        "https://eaglecrestwood.modebills.com/physical-and-health-education-jss3/",
-      "AGRICULTURAL SCIENCE":
-        "https://eaglecrestwood.modebills.com/agricultural-science-jss3/",
-      "CODING & ROBOTICS":
-        "https://eaglecrestwood.modebills.com/coding-robotics-jss3/",
-      "LITERATURE-IN-ENGLISH":
-        "https://eaglecrestwood.modebills.com/literature-in-english-jss3/",
-      "COMPUTER SCIENCE":
-        "https://eaglecrestwood.modebills.com/computer-science-jss3/",
-      "BASIC TECHNOLOGY":
-        "https://eaglecrestwood.modebills.com/basic-technology-jss3/",
+        "https://eaglecrestwood.modebills.com/socialstudies-jss3/",
     },
   },
-  sss_section: {
+  "sss section": {
     SS1: {
       /* Add subjects & links */
     },
